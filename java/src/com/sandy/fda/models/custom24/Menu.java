1f1a47bc47232f33ddf48f4524774902d68333ab
Santhosh Swamy V <santhoshswamyv@gmail.com>
2026-08-20 13:06:50 +0530

    missed: Added Fields List

--- java/src/com/sandy/fda/models/custom24/Menu.java
+++ java/src/com/sandy/fda/models/custom24/Menu.java
@@ -6,9 +6,10 @@ import com.sandy.fda.models.custom24.enums.FileType;
 import com.sandy.fda.models.custom24.enums.MenuType;
 
 public record Menu(
-        MenuType menuType,
         String menuName,
         String menuDescription,
+        MenuType menuType,
+        List<Field> fields,
         List<FileType> generateDesign
 ) {
 }
