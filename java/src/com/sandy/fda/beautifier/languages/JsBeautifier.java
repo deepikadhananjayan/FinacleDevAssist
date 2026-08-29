9abecd75aeb22c63b06c1699a1fb81ab980f9c60
Santhosh Swamy V <santhoshswamyv@gmail.com>
2026-08-29 22:33:50 +0530

    fix: preserving new lines and removing unwanted lines while beautifing

--- java/src/com/sandy/fda/beautifier/languages/JsBeautifier.java
+++ java/src/com/sandy/fda/beautifier/languages/JsBeautifier.java
@@ -4,12 +4,17 @@ import com.sandy.fda.beautifier.ICodeBeautifier;
 import com.sandy.fda.models.beautifier.BeautifyData;
 
 import io.beautifier.javascript.JavaScriptBeautifier;
+import io.beautifier.javascript.JavaScriptOptions;
 
 public class JsBeautifier implements ICodeBeautifier {
 
     @Override
     public String beautify(BeautifyData beautifyData) throws Exception {
         String uglyCode = beautifyData.getContent();
-        return new JavaScriptBeautifier(uglyCode).beautify();
+        JavaScriptOptions options = JavaScriptOptions.builder()
+                .preserve_newlines(true)
+                .max_preserve_newlines(2)
+                .build();
+        return new JavaScriptBeautifier(uglyCode, options).beautify();
     }
 }
